3b0de34ac7e73c6fff80054fb31b1fcc8235b98f
adarshEr2020 <adarshprajapati31@gmail.com>
2021-06-28 11:33:04 +0530

    insert 40 after 30 to linked list sequence of 56

--- src/com/bridgelabz/linkedlist/LinkedListOperations.java
+++ src/com/bridgelabz/linkedlist/LinkedListOperations.java
@@ -1,103 +1,115 @@
 package com.bridgelabz.linkedlist;
 
 public class LinkedListOperations {
-	//Represent the head and tail of the singly linked list
+
 	INode head;
 	INode tail;
 	int size;
 
-	public void addNodeInBeginning(Node newNode) {
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            INode temp = head;
-            head = newNode;
-            head.setNext(temp);
-        }
-    }
+	public void addNodeInBeginnig(INode newNode) {
 
-    public void addNodeAtEnd(Node newNode) {
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail.setNext(newNode);
-            tail = newNode;
-        }
-        size++;
-    }
+		if(head == null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			INode temp = head;
+			head = newNode;
+			head.setNext(temp);
+		}
+		size++;
+	}
 
-    public void addNodeInMiddle(Node newNode) {
+	public void addNodeAtEnd(Node newNode) {
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			tail.setNext(newNode);
+			tail = newNode;
+		}
+		size++;
+	}
 
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            INode temp, current = null;
-            int count = (size % 2 == 0) ? (size / 2) : ((size + 1) / 2);
-            temp = head;
-            //current = null;
-            for (int i = 0; i < count; i++) {
-                current = temp;
-                temp = temp.getNext();
-            }
-            current.setNext(newNode);
-            newNode.setNext(temp);
-        }
-        size++;
-    }
+	public void addNodeInPosition(Integer element, int position) {
+		if (position < 0 || position > size)
+			System.out.println("Index out of bound!");
+		else if(position==1){
+			Node newNode = new Node(element);
+			newNode.setNext(head.getNext());
+			head.setNext(newNode);
+		}
+		else {
+			INode currentNode = head;
+			while (currentNode != null){
+				if(currentNode.getKey().equals(30)) {
+					Node newNode = new Node(element);
+					newNode.setNext(currentNode.getNext());
+					currentNode.setNext(newNode);
+					break;
+				}
+				currentNode = currentNode.getNext();
+			}
+		}
+		size ++;
+	}
 
-    public void deleteNodeFromBeginning() {
-        if (head == null)
-            System.out.println("Linked List is empty!");
-        else
-            head = head.getNext();
+	public void deleteNodeFromBeginning() {
+		if(head == null)
+			System.out.println("Linked List is empty!");
+		else
+			head = head.getNext();
 
-        size--;
-    }
+		size--;
+	}
 
-    public void deleteNodeFromLast() {
-        if (head == null)
-            System.out.println("Linked List is empty!");
-        else {
-            INode current = head;
-            while (tail.getNext() != null)
-                current = current.getNext();
-            current.setNext(null);
-        }
-        size--;
-    }
-    public void searchElement() {
-        if(head == null)
-            System.out.println("Linked List is empty");
-        else{
-            INode currentNode = head;
-            int position = 0;
-            while (currentNode != null){
-                position++;
-                if(currentNode.getKey().equals(30)){
-                    System.out.println("Key value 30 is present at position "+position+" in list");
-                }
-                currentNode = currentNode.getNext();
-            }
-        }
-    }
+	public void deleteNodeFromLast() {
+		if(head == null)
+			System.out.println("Linked List is empty!");
+		else {
+			INode current = head;
+			while(tail.getNext() != null)
+				current = current.getNext();
+			current.setNext(null);
+		}
+		size--;
+	}
 
-    public void displayNode() {
-        INode current = head;
-        if(head == null)
-            System.out.println("Linked List is empty");
-        System.out.print("Nodes are: " );
-        while (current != null) {
-            if(current.getNext() != null) {
-                System.out.print(current.getKey() + " -> ");
-                current = current.getNext();
-            }
-            else {
-                System.out.println(current.getKey());
-                current = current.getNext();
-            }
-        }
-    }
+	public void searchElement() {
+		if(head == null)
+			System.out.println("Linked List is empty");
+		else{
+			INode currentNode = head;
+			int position = 0;
+			while (currentNode != null){
+				position++;
+				if(currentNode.getKey().equals(30)){
+					System.out.println("Key value 30 is present at position "+position+" in list");
+				}
+				currentNode = currentNode.getNext();
+			}
+		}
+	}
+
+	public void displayNode(INode node) {
+		INode current = head;
+
+		if(head == null) {
+			System.out.println("List is empty");
+			return;
+		}
+		System.out.println("Nodes of singly linked list: ");
+		while(current != null) {
+			if(current.getNext() != null) {
+				System.out.print(current.getKey() + " -> ");
+				current = current.getNext();
+			}
+			else {
+				System.out.print(current.getKey());
+				break;
+			}
+		}
+		System.out.println();
+	}
 }
